502fd080f5c6b7a241ef3d67d406581346d5b121
William Roe <git@wjlr.org.uk>
2019-01-01 18:06:22 +0000

    Open a blank window

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [ "`uname -s`" == "Darwin" ]; then
+  CFLAGS="-I/opt/X11/include `pkg-config --cflags glfw3` `pkg-config --libs glfw3` -framework OpenGL"
+else
+  CFLAGS="-lGL -lglfw -lX11 -lpthread -lXrandr -lXi -ldl"
+fi
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$DIR" #> /dev/null
+mkdir -p ./build
+pushd ./build #> /dev/null
+c++ -std=c++11 -o learngl ../src/glad.c ../src/main.cpp -I../include -I/usr/local/include -g $CFLAGS
---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, width, height);
+}
+
+void error_callback(int error, const char *description)
+{
+  std::cout << description << std::endl;
+}
+
+int main()
+{
+  glfwInit();
+  glfwSetErrorCallback(error_callback);
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // for macOS
+  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
