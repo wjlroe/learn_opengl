ded165ea5eb5672a90978f448fcbd80ce0d9ad53
William Roe <git@wjlr.org.uk>
2019-01-03 14:01:55 +0000

    Render a color gradient across the rect

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,25 @@
 
 static const char *vertexShaderSource = "#version 330 core \n\
 layout (location = 0) in vec3 aPos;\n\
+layout (location = 1) in vec3 aColor;\n\
+\n\
+out vec3 ourColor;\n\
 \n\
 void main() \n\
 { \n\
   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n\
+  ourColor = aColor;\n\
 } \n\
 ";
 
 static const char *fragmentShaderSource = "#version 330 core\n\
 out vec4 FragColor;\n\
 \n\
-uniform vec4 ourColor;\n\
+in vec3 ourColor;\n\
 \n\
 void main()\n\
 {\n\
-  FragColor = ourColor;\n\
+  FragColor = vec4(ourColor, 1.0);\n\
 }\n\
 ";
 
@@ -51,10 +55,10 @@ void process_input(GLFWwindow *window)
 DrawResources setupDrawResources()
 {
   float vertices[] = {
-      0.5f, 0.5f, 0.0f,   // top right
-      0.5f, -0.5f, 0.0f,  // bottom right
-      -0.5f, -0.5f, 0.0f, // bottom left
-      -0.5f, 0.5f, 0.0f}; // top left
+      0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // top right
+      0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, // bottom left
+      -0.5f, 0.5f, 0.0f, 0.5f, 0.5f, 0.5f}; // top left
   unsigned int indices[] = {
       0, 1, 3,
       1, 2, 3};
@@ -77,10 +81,14 @@ DrawResources setupDrawResources()
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
   int aPosLocation = 0;
+  int aColorLocation = 1;
   int aPosSize = 3;
+  int aColorSize = 3;
 
-  glVertexAttribPointer(aPosLocation, aPosSize, GL_FLOAT, GL_FALSE, aPosSize * sizeof(float), (void *)0);
+  glVertexAttribPointer(aPosLocation, aPosSize, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(aPosLocation);
+  glVertexAttribPointer(aColorLocation, aColorSize, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(aColorLocation);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
