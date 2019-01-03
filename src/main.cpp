7802bfed7918f4cac779365361d0d75583456a4b
William Roe <git@wjlr.org.uk>
2019-01-03 13:21:40 +0000

    Draw a rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -41,9 +41,13 @@ void process_input(GLFWwindow *window)
 unsigned int setupVAO()
 {
   float vertices[] = {
-      -0.5f, -0.5f, 0.0f,
-      0.5f, -0.5f, 0.0f,
-      0.0f, 0.5f, 0.0f};
+      0.5f, 0.5f, 0.0f,   // top right
+      0.5f, -0.5f, 0.0f,  // bottom right
+      -0.5f, -0.5f, 0.0f, // bottom left
+      -0.5f, 0.5f, 0.0f}; // top left
+  unsigned int indices[] = {
+      0, 1, 3,
+      1, 2, 3};
 
   unsigned int VAO;
   glGenVertexArrays(1, &VAO);
@@ -56,23 +60,32 @@ unsigned int setupVAO()
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+  unsigned int EBO;
+  glGenBuffers(1, &EBO);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
   int aPosLocation = 0;
   int aPosSize = 3;
 
   glVertexAttribPointer(aPosLocation, aPosSize, GL_FLOAT, GL_FALSE, aPosSize * sizeof(float), (void *)0);
   glEnableVertexAttribArray(aPosLocation);
 
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
   return VAO;
 }
 
-void drawTriangle(unsigned int shaderProgram, unsigned int VAO)
+void drawRectangle(unsigned int shaderProgram, unsigned int VAO)
 {
   glUseProgram(shaderProgram);
   glBindVertexArray(VAO);
-  // draw here
-  int firstTriangle = 0;
-  int numTriangles = 3;
-  glDrawArrays(GL_TRIANGLES, firstTriangle, numTriangles);
+  // Draw in wireframe mode
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  int numTriangles = 6;
+  glDrawElements(GL_TRIANGLES, numTriangles, GL_UNSIGNED_INT, 0);
 }
 
 void render(unsigned int shaderProgram, unsigned int VAO)
@@ -80,7 +93,7 @@ void render(unsigned int shaderProgram, unsigned int VAO)
   glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
 
-  drawTriangle(shaderProgram, VAO);
+  drawRectangle(shaderProgram, VAO);
 }
 
 int main()
