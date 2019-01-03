6beb431bb3c38226fa43c5f582f124b39b40149d
William Roe <git@wjlr.org.uk>
2019-01-03 13:45:33 +0000

    Send color from vertex to fragment shader

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,23 @@
 static const char *vertexShaderSource = "#version 330 core \n\
 layout (location = 0) in vec3 aPos;\n\
 \n\
+out vec4 vertexColor;\n\
+\n\
 void main() \n\
 { \n\
   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n\
+  vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n\
 } \n\
 ";
 
 static const char *fragmentShaderSource = "#version 330 core\n\
 out vec4 FragColor;\n\
 \n\
+in vec4 vertexColor;\n\
+\n\
 void main()\n\
 {\n\
-  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0);\n\
+  FragColor = vertexColor;\n\
 }\n\
 ";
 
