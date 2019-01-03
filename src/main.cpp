8af606e9f657f982991902d648be768566038bf2
William Roe <git@wjlr.org.uk>
2019-01-03 13:09:48 +0000

    Draw a triangle

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,24 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+static const char *vertexShaderSource = "#version 330 core \n\
+layout (location = 0) in vec3 aPos;\n\
+\n\
+void main() \n\
+{ \n\
+  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0); \n\
+} \n\
+";
+
+static const char *fragmentShaderSource = "#version 330 core\n\
+out vec4 FragColor;\n\
+\n\
+void main()\n\
+{\n\
+  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0);\n\
+}\n\
+";
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
   glViewport(0, 0, width, height);
@@ -20,10 +38,49 @@ void process_input(GLFWwindow *window)
   }
 }
 
-void render()
+unsigned int setupVAO()
+{
+  float vertices[] = {
+      -0.5f, -0.5f, 0.0f,
+      0.5f, -0.5f, 0.0f,
+      0.0f, 0.5f, 0.0f};
+
+  unsigned int VAO;
+  glGenVertexArrays(1, &VAO);
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  int aPosLocation = 0;
+  int aPosSize = 3;
+
+  glVertexAttribPointer(aPosLocation, aPosSize, GL_FLOAT, GL_FALSE, aPosSize * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(aPosLocation);
+
+  return VAO;
+}
+
+void drawTriangle(unsigned int shaderProgram, unsigned int VAO)
+{
+  glUseProgram(shaderProgram);
+  glBindVertexArray(VAO);
+  // draw here
+  int firstTriangle = 0;
+  int numTriangles = 3;
+  glDrawArrays(GL_TRIANGLES, firstTriangle, numTriangles);
+}
+
+void render(unsigned int shaderProgram, unsigned int VAO)
 {
   glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
+
+  drawTriangle(shaderProgram, VAO);
 }
 
 int main()
@@ -58,10 +115,57 @@ int main()
 
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+              << infoLog << std::endl;
+  }
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR::PROGRAM::LINK_FAILED\n"
+              << infoLog << std::endl;
+  }
+
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  unsigned int VAO = setupVAO();
+
   while (!glfwWindowShouldClose(window))
   {
     process_input(window);
-    render();
+    render(shaderProgram, VAO);
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
