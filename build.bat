4049ed5f71e9b0f33913ea4fe673f3d2bfdbd076
William Roe <git@wjlr.org.uk>
2020-05-26 23:23:32 +0100

    Try building on Windows as a GUI app

--- build.bat
+++ build.bat
@@ -86,7 +86,7 @@ REM SET CL=/DGEN_CONVERT_ELEMENTS_FROM_VERTICES#1
 cl /std:c++17 -FC -Zc:strictStrings -Zi -EHsc -diagnostics:column %build_flags:"=% ..\src\main.cpp -I ..\include^
  -I %GLFW_INCLUDE_PATH% -I %ASSIMP_INCLUDE_PATH% -I ..\include\imgui -I ..\include\stb -I ..\include\simpleini^
  /link %GLFW_LIB_PATH%\glfw3.lib %GLFW_LIB_PATH%\glfw3dll.lib opengl32.lib^
- glu32.lib %ASSIMP_LIB_PATH%\assimp-vc140-mt.lib /SUBSYSTEM:CONSOLE
+ glu32.lib user32.lib gdi32.lib %ASSIMP_LIB_PATH%\assimp-vc140-mt.lib
 IF %ERRORLEVEL% NEQ 0 SET /A errno=%ERRORLEVEL%
 popd
 
