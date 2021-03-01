c2c114cf0176004bbdc0f25e15a367dcfc96c0e4
William Roe <git@wjlr.org.uk>
2021-03-01 16:55:38 +0000

    re-indent

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,21 +1,21 @@
 char* LoadFileContents(const char* Path)
 {
-    std::string Contents;
-    std::ifstream file;
-    file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-    try {
-        file.open(Path);
-        std::stringstream fileStream;
+  std::string Contents;
+  std::ifstream file;
+  file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+  try {
+    file.open(Path);
+    std::stringstream fileStream;
 
-        fileStream << file.rdbuf();
+    fileStream << file.rdbuf();
 
-        file.close();
+    file.close();
 
-        Contents = fileStream.str();
-    } catch (const std::ifstream::failure& e) {
-        std::cerr << "ERROR::FILE_NOT_SUCCESSFULLY_READ : " << Path
-            << std::endl;
-        throw;
-    }
-    return strdup(Contents.c_str());
+    Contents = fileStream.str();
+  } catch (const std::ifstream::failure& e) {
+    std::cerr << "ERROR::FILE_NOT_SUCCESSFULLY_READ : " << Path
+      << std::endl;
+    throw;
+  }
+  return strdup(Contents.c_str());
 }
